86022af243eabd5a2cf8b24c2126fd4e29cd9e99
Manuel <manugonzalez.tech@gmail.com>
2025-01-22 14:11:11 +0000

    Add lists and keys demo

---
+++ demos/week6/lists-and-keys/src/components/Section.jsx
@@ -0,0 +1,8 @@
+export default function Section(props) {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+    </>
+  );
+}
---
+++ demos/week6/lists-and-keys/src/lib/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    id: 1,
+    title: "Section 1",
+    content: "Content 1",
+  },
+  {
+    id: 2,
+    title: "Section 2",
+    content: "Content 2",
+  },
+  {
+    id: 3,
+    title: "Section 3",
+    content: "Content 3",
+  },
+  {
+    id: 4,
+    title: "Section 4",
+    content: "Content 4",
+  },
+  {
+    id: 5,
+    title: "Section 5",
+    content: "Content 5",
+  },
+];
