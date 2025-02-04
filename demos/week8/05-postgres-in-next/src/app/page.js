ac4fd8d05d916f4ba546377280ae97669b7779f6
Manuel <manugonzalez.tech@gmail.com>
2025-02-04 15:44:49 +0000

    Add postgres demo

---
+++ demos/week8/05-postgres-in-next/src/app/page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <>
+      {/* Escape your apostrophes for a stress-free Vercel deployment */}
+      <h1>Joe&apos;s Birds</h1>
+      <h2>Where birds are happy and fluffy</h2>
+    </>
+  );
+}
