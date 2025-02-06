d1762afbe255f203cb4d01e4d72c6982493455f8
Joe Venton <joe.venton@techeducators.co.uk>
2025-02-06 15:20:16 +0000

    added walking skeleton

---
+++ demos/week8/wk8--walking-skeleton/src/app/posts/page.js
@@ -0,0 +1,11 @@
+// give your user some navigation controls!
+// remember to add metadata for the page!
+// we need to be able to sort the posts by asc and desc. Query strings!!!
+
+export default function PostsPage() {
+  return (
+    //i need to get all posts from my database!
+    <h1>Posts Page</h1>
+    // i need to render all of the posts!
+  );
+}
